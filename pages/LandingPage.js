911483b9bce145d87728af762bbe6df88a060f74
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-05-23 17:35:26 +0100

    created the landing page

--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -6,11 +6,10 @@ const Landing = () => {
         <Header titleClassName="text-5xl absolute left-0 top-60 transform translate-x-6 translate-y-58 text-blue-700" title="Welcome to Bridging Barriers Portal" />
         <div className="mt-60 w-2/4">
           <hr className="border-blue-700 border-4" />
-          <div className="mr-5">
           <img className="w-1/2" src="../Mentor.jpg" alt="Mentor Image" />
         </div>
         </div>
-      </div>
+    
     );
   }
   
