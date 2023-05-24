0e9caba28ae1b196cd696537169254477c293f34
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-05-24 13:34:44 +0100

    added image and styled the buttons

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,15 @@
 import Link from "next/link";
-import LargeButton from "./largeButton";
+import NormalButton from "@/components/buttons/NormalButton";
 
-
-
-
-
-const Navbar = ()=> {
-    return (
-        <nav className="flex items-center justify-between p-6 bg-white">
-            <div className="text-white text-2xl">
-            <img src="../logo.png" alt="Logo"  className="w-18 h-8 mr-2" />
-            </div>
-            <LargeButton/>
-        </nav>
-    )
-}
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between p-6 bg-gray-200">
+      <div className="text-white text-2xl">
+        <img src="../logo.png" alt="Logo" className="w-22 h-12 mr-2" />
+      </div>
+      <NormalButton text="Login" />
+    </nav>
+  );
+};
 
 export default Navbar;
--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -1,16 +1,31 @@
-import Header from "../components/Header"
+import LargeButton from "@/components/buttons/LargeButton";
+import Header from "../components/Header";
+import "@/app/globals.css";
 
 const Landing = () => {
-    return (
-      <div>
-        <Header titleClassName="text-5xl absolute left-0 top-60 transform translate-x-6 translate-y-58 text-blue-700" title="Welcome to Bridging Barriers Portal" />
-        <div className="mt-60 w-2/4">
-          <hr className="border-blue-700 border-4" />
-          <img className="w-1/2" src="../Mentor.jpg" alt="Mentor Image" />
-        </div>
-        </div>
-    
-    );
-  }
-  
-  export default Landing;
+  return (
+    <div className="relative">
+      <Header
+        titleClassName="text-5xl absolute left-0 top-20 transform translate-x-6 translate-y-58 text-blue-500"
+        title="Welcome to the Bridging Barriers Portal"
+      />
+      <div className="mx-auto">
+        <hr className="border-blue-500 border-2 mt-40 w-3/4 rounded" />
+      </div>
+      <div className="flex">
+        <img
+          className="ml-auto mr-10 w-2/6 mt-20 rounded-25 shadow-2xl"
+          style={{ transform: "translate(-60px, -30px)" }}
+          src="../Mentor.jpg"
+          alt="Mentor Image"
+        />
+      </div>
+      <div className="absolute inset-0 flex items-start justify-start flex-col ml-10 mt-80">
+        <LargeButton className="mb-16" text="Create Profile" />
+        <LargeButton text="Login" />
+      </div>
+    </div>
+  );
+};
+
+export default Landing;
