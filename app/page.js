53a50c550e8452d4f3c9886708e298241458c52c
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-05-24 19:20:54 +0100

    final draft of landing page complete

--- app/page.js
+++ app/page.js
@@ -1,14 +1,13 @@
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
-import Landing from '@/pages/LandingPage'
-
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import Landing from "@/pages/LandingPage";
 
 export default function Home() {
   return (
-    <div className='bg-white min-h-screen' >
-   <Navbar/>
-   <Footer/>
-   <Landing/>
+    <div className="bg-blue-100 min-h-screen rounded">
+      <Navbar />
+      <Footer />
+      <Landing />
     </div>
-  )
+  );
 }
--- pages/LandingPage.js
+++ pages/LandingPage.js
@@ -1,6 +1,6 @@
 import LargeButton from "@/components/buttons/LargeButton";
 import Header from "../components/Header";
-import "@/app/globals.css";
+import SubHeader from "@/components/subheader";
 
 const Landing = () => {
   return (
@@ -10,19 +10,34 @@ const Landing = () => {
         title="Welcome to the Bridging Barriers Portal"
       />
       <div className="mx-auto">
-        <hr className="border-blue-500 border-2 mt-40 w-3/4 rounded" />
+        <hr className="mt-40 w-1/2 rounded bg-gradient-to-r from-blue-500 to-blue-100 h-8" />
       </div>
+
+      <SubHeader
+        titleClassName="text-3xl absolute left-40 transform translate-x-6 translate-y-10 text-blue-500"
+        title="Developing lasting relationships"
+      />
       <div className="flex">
-        <img
-          className="ml-auto mr-10 w-2/6 mt-20 rounded-25 shadow-2xl"
-          style={{ transform: "translate(-60px, -30px)" }}
-          src="../Mentor.jpg"
-          alt="Mentor Image"
-        />
+        <div style={{ width: "100%", height: "400px" }}>
+          <img
+            className="ml-auto mr-10 mt-20 rounded-25 shadow-2xl"
+            style={{
+              transform: "translate(-60px, -90px)",
+              objectFit: "cover",
+              height: "100%",
+            }}
+            src="../Mentor.jpg"
+            alt="Mentor Image"
+          />
+        </div>
       </div>
-      <div className="absolute inset-0 flex items-start justify-start flex-col ml-10 mt-80">
-        <LargeButton className="mb-16" text="Create Profile" />
-        <LargeButton text="Login" />
+
+      <div
+        className="absolute inset-0 flex items-start justify-start flex-col ml-40 mt-80"
+        style={{ transform: "translate(-10px, 50px)" }}
+      >
+        <LargeButton className="mb-16 shadow-2xl" text="Create Profile" />
+        <LargeButton className="shadow-2xl" text="Login" />
       </div>
     </div>
   );
