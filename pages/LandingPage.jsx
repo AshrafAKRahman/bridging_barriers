325ba1aa2c3b374c9a570c5f981d45eb142a8a05
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2023-05-29 14:19:18 +0100

    started styling to be mobile responsive

--- pages/LandingPage.jsx
+++ pages/LandingPage.jsx
@@ -6,7 +6,7 @@ const Landing = () => {
   return (
     <div className="relative">
       <Header
-        titleClassName="text-5xl absolute left-0 top-20 transform translate-x-6 translate-y-58 text-blue-500"
+        titleClassName="text-lg absolute left-0 right-10 top-10 transform translate-x-6 translate-y-58 text-blue-500"
         title="Welcome to the Bridging Barriers Portal"
       />
       <div className="mx-auto">
@@ -14,28 +14,22 @@ const Landing = () => {
       </div>
 
       <SubHeader
-        titleClassName="text-3xl absolute left-40 transform translate-x-6 translate-y-10 text-blue-500"
+        titleClassName="text-3xl absolute left-0  mt-10 text-blue-500"
         title="Developing lasting relationships"
       />
-      <div className="flex">
-        <div style={{ width: "100%", height: "400px" }}>
-          <img
-            className="ml-auto mr-10 mt-20 rounded-25 shadow-2xl"
-            style={{
-              transform: "translate(-60px, -90px)",
-              objectFit: "cover",
-              height: "100%",
-            }}
-            src="../Mentor.jpg"
-            alt="Mentor Image"
-          />
-        </div>
+      <div className="w-full md:w-1/2 hidden md:block">
+        <img
+          className="mx-auto md:ml-auto md:mr-10 mt-20 rounded-25 shadow-2xl"
+          style={{
+            transform: "translateY(-90px)",
+            objectFit: "cover",
+            height: "400px",
+          }}
+          src="../Mentor.jpg"
+          alt="Mentor Image"
+        />
       </div>
-
-      <div
-        className="absolute inset-0 flex items-start justify-start flex-col ml-40 mt-80"
-        style={{ transform: "translate(-10px, 50px)" }}
-      >
+      <div className="w-full md:w-1/2 flex flex-col items-start justify-start mt-60 md:mt-80 ml-20">
         <LargeButton
           className="mb-16 shadow-2xl"
           text="Create Profile"
